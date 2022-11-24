719ed4ab60b4d1bc7d02af953b1f4e60a877c4ce
Priyanka Chaurasia <chaurasia.p@northeastern.edu>
2022-11-24 17:28:10 -0500

    DB connection and Initial table entry

--- src/finalproject/FinalProject.java
+++ src/finalproject/FinalProject.java
@@ -13,8 +13,8 @@ public class FinalProject {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+//    public static void main(String[] args) {
+//        // TODO code application logic here
+//    }
     
 }
