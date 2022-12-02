e6678f42fb2a7d1e3f9dbf0666c324b71eb230b0
Navin <sharma.navi@northeastern.edu>
2022-12-01 20:21:56 -0500

    system admin updated

--- src/System_Admin/System_Admin.java
+++ src/System_Admin/System_Admin.java
@@ -21,4 +21,53 @@ public class System_Admin
     private CoursesDirectory courses_directory;
     private FieldOfInterest field_of_interest_directory;
     private UniversityDirectory university_directory;
+    
+    public System_Admin()
+    {
+        this.users_directory = users_directory;
+        this.student_directory = student_directory;
+        this.courses_directory = courses_directory;
+        this.field_of_interest_directory = field_of_interest_directory;
+        this.university_directory = university_directory;
+    }
+
+    public UsersDirectory getUsers_directory() {
+        return users_directory;
+    }
+
+    public void setUsers_directory(UsersDirectory users_directory) {
+        this.users_directory = users_directory;
+    }
+
+    public StudentDirectory getStudent_directory() {
+        return student_directory;
+    }
+
+    public void setStudent_directory(StudentDirectory student_directory) {
+        this.student_directory = student_directory;
+    }
+
+    public CoursesDirectory getCourses_directory() {
+        return courses_directory;
+    }
+
+    public void setCourses_directory(CoursesDirectory courses_directory) {
+        this.courses_directory = courses_directory;
+    }
+
+    public FieldOfInterest getField_of_interest_directory() {
+        return field_of_interest_directory;
+    }
+
+    public void setField_of_interest_directory(FieldOfInterest field_of_interest_directory) {
+        this.field_of_interest_directory = field_of_interest_directory;
+    }
+
+    public UniversityDirectory getUniversity_directory() {
+        return university_directory;
+    }
+
+    public void setUniversity_directory(UniversityDirectory university_directory) {
+        this.university_directory = university_directory;
+    }
 }
